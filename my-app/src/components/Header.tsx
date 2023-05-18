960a9673531a2a721299a49efaa104a1c4035b57
Vseslav Rashchynski <vrashinskiyv@gmail.com>
2023-05-18 13:06:07 +0200

    feat: start project

---
+++ my-app/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import CrossedEllipsesIcon from './CrossedEllipsesIcon';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="title">
+        <CrossedEllipsesIcon />
+        <h1>Jobored</h1>
+      </div>
+      <nav></nav>
+    </header>
+  );
+};
+
+export default Header;
