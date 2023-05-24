dd788820dc60a0bd37da33a99750fb476ed9a2b7
Vseslav Rashchynski <vrashinskiyv@gmail.com>
2023-05-24 14:22:49 +0200

    feat: implemente pages in favorites

--- my-app/src/components/Favorites/Favorites.tsx
+++ my-app/src/components/Favorites/Favorites.tsx
@@ -1,18 +1,43 @@
 import JobCard from '../MainContent/JobCard';
-import { Vacancy } from '../constants';
+import { DEF_VAC, Vacancy } from '../constants';
 import EmptyPage from '../EmptyPageFavorites';
+import { Pagination } from '@mantine/core';
+import { useState } from 'react';
 
 const Favorites = () => {
+  const [activePage, setActivePage] = useState(1);
+
   const savedItems = JSON.parse(localStorage.getItem('savedItems') || '[]');
+  const totalPages = savedItems.length / DEF_VAC;
+  const startIndex = (activePage - 1) * DEF_VAC;
+  const endIndex = startIndex + DEF_VAC;
+  const displayedVacancies = savedItems.slice(startIndex, endIndex);
+
+  const handlePageChange = (page: number) => {
+    setActivePage(page);
+  };
   return (
     <div className="favorites">
       {savedItems.length === 0 ? (
         <EmptyPage />
       ) : (
         <>
-          {savedItems.map((el: Vacancy) => (
+          {displayedVacancies.map((el: Vacancy) => (
             <JobCard key={el.id} el={el} />
           ))}
+
+          {totalPages > 1 && (
+            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '1rem' }}>
+              <Pagination
+                total={totalPages}
+                value={activePage}
+                onChange={handlePageChange}
+                size="sm"
+                radius="sm"
+                variant="outline"
+              />
+            </div>
+          )}
         </>
       )}
     </div>
